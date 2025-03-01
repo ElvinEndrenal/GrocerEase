ca602e74abfccecb17c74a5442f04e79df66bd15
elvin.codes <155425194+ElvinEndrenal@users.noreply.github.com>
2025-03-01 10:53:32 +0800

    insert

--- app/src/main/java/com/example/grocerease/MainActivity.java
+++ app/src/main/java/com/example/grocerease/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.grocerease;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -34,5 +36,15 @@ public class MainActivity extends AppCompatActivity {
         aboutButton = findViewById(R.id.aboutButton);
         search = findViewById(R.id.search);
         listView = findViewById(R.id.listView);
+
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Add.class);
+                startActivity(intent);
+            }
+        });
     }
 }
